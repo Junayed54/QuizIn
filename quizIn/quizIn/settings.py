27498c604e8099fc3c473f61b251976c3cdadede
Junayed-shanto <junayedshanto54@gmail.com>
2024-10-03 00:11:39 +0600

    sqlite connect

--- quizIn/quizIn/settings.py
+++ quizIn/quizIn/settings.py
@@ -80,24 +80,24 @@ WSGI_APPLICATION = 'quizIn.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',  # Use 'mysql' for MySQL
-        'NAME': 'quizIn',              # Your database name
-        'USER': 'root',                       # Your MySQL username
-        'PASSWORD': 'root123',             # Your MySQL password
-        'HOST': 'localhost',                    # Your database host
-        'PORT': '3306',                         # Default MySQL port
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',  # Use 'mysql' for MySQL
+#         'NAME': 'quizIn',              # Your database name
+#         'USER': 'root',                       # Your MySQL username
+#         'PASSWORD': 'root123',             # Your MySQL password
+#         'HOST': 'localhost',                    # Your database host
+#         'PORT': '3306',                         # Default MySQL port
+#     }
+# }
 
 
 # Password validation
